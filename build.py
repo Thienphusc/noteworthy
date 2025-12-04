640a73b9c132136dec5504ac3fd3333ec1151724
R0K0R <rkr0k0r@gmail.com>
2025-12-04 23:10:06 +0900

    build.py bug fix

--- build.py
+++ build.py
@@ -67,7 +67,6 @@ def compile_target(target, output_path, page_offset=None, page_map=None, quiet=T
         RENDERER_FILE, 
         str(output_path),
         "--root", ".",
-        "--root", ".",
         "--input", f"target={target}"
     ]
     
